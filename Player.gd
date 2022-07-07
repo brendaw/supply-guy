cdb863e16ef4b51155182a95613b1795f9b23975
William Brendaw <williambrendaw@icloud.com>
2022-07-06 21:42:17 -0300

    Refactor Player and Mate to implement collision effect

--- Player.gd
+++ Player.gd
@@ -1,13 +1,15 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 200
+signal hit
+
+export var speed = 150
+
+var velocity = Vector2.ZERO
 
 # TO-DO: Implement AI for shoot and get behind the barricade
 # TO-DO: Implement the logic for running low of resources.
 
 var screen_size
-
-signal hit
 
 func start(pos):
 	position = pos
@@ -18,9 +20,18 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 	
-func _process(delta):
-	var velocity = Vector2.ZERO
+func _physics_process(delta):
+	get_input()
+	animate_player()
 	
+	move_and_slide(velocity)
+	
+	clear_velocity()
+
+func key_pressed(key):
+	return Input.is_action_pressed(key)
+
+func get_input():
 	if key_pressed("move_right"):
 		velocity.x += 1
 	
@@ -33,6 +44,9 @@ func _process(delta):
 	if key_pressed("move_down"):
 		velocity.y += 1
 	
+	velocity = velocity.normalized() * speed
+
+func animate_player():
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -43,14 +57,6 @@ func _process(delta):
 		$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.flip_h = false
-	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
 
-func key_pressed(key):
-	return Input.is_action_pressed(key)
-
-func _on_Player_body_entered(body):
-	emit_signal("hit")
-	$CollisionShape2D.set_deferred("disabled", true)
+func clear_velocity():
+	velocity = Vector2.ZERO
